765c9df8edd4e455d1f92ff949eef5b9cbf2e2ab
samuelkime267 <samuelkime7@gmail.com>
2025-06-03 15:34:46 +0100

    fixed padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full z-50 p-4 flex items-center justify-between opacity-0 pointer-events-none">
       <Nav />
-      <div className="lg:mx-auto pt-3">
+      <div className="lg:mx-auto pt-1">
         <Logo className="h-6 text-white" />
       </div>
       <Languages />
